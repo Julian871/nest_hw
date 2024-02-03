8851a2de42f4d88f4e5a7d841c93bacba5c2b019
Julian871 <vindiesel.1999@mail.ru>
2024-02-03 13:48:18 +0300

    correct query

--- src/features/posts/default-query.ts
+++ src/features/posts/default-query.ts
@@ -1,5 +1,15 @@
+import { Transform } from 'class-transformer';
+
 export class PostsDefaultQuery {
   sortBy: string = 'createdAt';
+
+  @Transform(({ value }) => {
+    if (value.toLowerCase() === 'asc') {
+      return 'ASC';
+    } else {
+      return 'DESC';
+    }
+  })
   sortDirection: 'DESC' | 'ASC' = 'DESC';
   pageNumber: number = 1;
   pageSize: number = 10;
